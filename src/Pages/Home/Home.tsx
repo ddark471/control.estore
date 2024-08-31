58513d56f4fe469e8703e6195ca68b813ad679f8
ddark471 <ddarkk471@gmail.com>
2024-08-31 19:40:18 +0500

    feat(Home): Home page folder

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./home.module.scss"
+
+const Home = () => {
+  return (
+    <div className={style.wrapper}>
+        
+    </div>
+  )
+}
+
+export default Home
