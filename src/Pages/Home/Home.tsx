66d0e83dea7ef13f2141cf5ffb30377b9a4564f3
ddark471 <ddarkk471@gmail.com>
2024-09-04 17:12:56 +0500

    fix(Home): deleted spaces

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import style from "./home.module.scss"
+import Sidebar from 'components/Sidebar'
+import Products from 'components/Products'
 
 const Home = () => {
   return (
     <div className={style.wrapper}>
-        
+        <Sidebar/>
+        <Products/>
     </div>
   )
 }
