df898e7e912d3a64269647988b84702eca64fb12
ddark471 <ddarkk471@gmail.com>
2024-09-05 11:53:14 +0500

    fix(Pages.tsx): improved login, only auth users can acces other pages

--- src/Pages/Pages.tsx
+++ src/Pages/Pages.tsx
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Home from './Home'
-import AddProducts from 'components/AddProducts'
+import AddProducts from './AddProducts'
 import style from "./pages.module.scss"
+import { AuthContext } from 'contexts/AuthContext'
 
 const Pages = () => {
+  const context = useContext(AuthContext);
+  /*
+    Login logic
+    share token from the hook useLogIn.
+    if token exists, let user go for other pages
+    else only login page
+    
+    1) sharing token => use context method for global sharing.
+    2) in pages, conditionally render pages.
+
+  */
+  if(!context?.authToken){
+    return (
+      <div className={style.wrapper}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
+  }
+
+
   return (
     <div className={style.wrapper}>
       <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<Login/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path='*' element={<h1>Wrong direction</h1>}/>
-          <Route path='/home/products/add' element={<AddProducts/>}/>
-        </Routes>
+        {
+          context.authToken && (  
+            <Routes>
+              <Route path="/home" element={<Home/>}/>
+              <Route path='/home/products/add' element={<AddProducts/>}/>
+              <Route path='*' element={<h1>Wrong direction</h1>}/>
+            </Routes>
+          )
+        }
+        {
+          !context.authToken && (
+            <Routes>
+              <Route path='/login' element={<Login/>}/>
+            </Routes>
+          )
+        }
       </BrowserRouter>
         
     </div>
